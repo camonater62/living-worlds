d5c1c0b00ba0ec23124e7a5288e50f6f232137c5
Cameron West <camonater62@gmail.com>
2023-05-23 21:00:02 -0700

    Change based on month

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,80 @@
-use chrono::{Local, Timelike};
-use json::JsonValue;
-use std::collections::{BTreeMap, HashMap};
-use std::fs::File;
-use std::io::prelude::*;
-
+use chrono::{Datelike, Local, Timelike};
 use glutin::dpi::PhysicalSize;
 use glutin::event::{Event, WindowEvent};
 use glutin::event_loop::{ControlFlow, EventLoop};
 use glutin::window::WindowBuilder;
 use glutin::ContextBuilder;
+use json::JsonValue;
+use rand::Rng;
+use std::collections::{BTreeMap, HashMap};
+use std::fs::File;
+use std::io::prelude::*;
 
-fn get_palette_time(timeline: &BTreeMap<u32, &str>, seconds: u32) -> u32 {
-    let mut last_time: &u32 = timeline.keys().last().unwrap();
-    for (time, _) in timeline.iter() {
-        if time > &seconds {
-            return *last_time;
-        }
-        last_time = time;
-    }
-    *timeline.keys().last().unwrap()
-}
-
-fn get_palette(
-    all_palettes: &JsonValue,
-    timeline: &BTreeMap<u32, &str>,
-    seconds: u32,
-) -> [[u8; 3]; 256] {
-    let time = get_palette_time(timeline, seconds);
-    let palette_raw: &JsonValue = &all_palettes[*timeline.get(&time).unwrap()];
-
-    let colors = palette_raw["colors"]
-        .members()
-        .map(|x| {
-            let r = x[0].as_u8().unwrap();
-            let g = x[1].as_u8().unwrap();
-            let b = x[2].as_u8().unwrap();
-            [r, g, b]
-        })
-        .collect::<Vec<[u8; 3]>>();
-    let mut palette = [[0; 3]; 256];
-    for (i, color) in colors.iter().enumerate() {
-        palette[i] = *color;
-    }
-    palette
+// Struct for a cycle of a palette
+struct Cycle {
+    reverse: u32,
+    rate: u32,
+    low: u32,
+    high: u32,
 }
 
 fn main() -> std::io::Result<()> {
     // Json processing
-    let mut file = File::open("scenes/April-Clear.json").unwrap();
+    let mut rng = rand::thread_rng();
+    let rand = rng.gen::<u32>();
+    let month = Local::now().month();
+    let mut file = match month {
+        1 => match rand % 2 {
+            0 => File::open("scenes/January-Clear.json").unwrap(),
+            1 => File::open("scenes/January-Snow.json").unwrap(),
+            _ => panic!("Lol aint no way"),
+        },
+        2 => match rand % 2 {
+            0 => File::open("scenes/February-Clear.json").unwrap(),
+            1 => File::open("scenes/February-Cloudy.json").unwrap(),
+            _ => panic!("Lol aint no way"),
+        },
+        3 => match rand % 1 {
+            0 => File::open("scenes/March-Clear.json").unwrap(),
+            _ => panic!("Lol aint no way"),
+        },
+        4 => match rand % 2 {
+            0 => File::open("scenes/April-Clear.json").unwrap(),
+            1 => File::open("scenes/April-Rain.json").unwrap(),
+            _ => panic!("Lol aint no way"),
+        },
+        5 => match rand % 3 {
+            0 => File::open("scenes/May-Clear.json").unwrap(),
+            1 => File::open("scenes/May-Cloudy.json").unwrap(),
+            2 => File::open("scenes/May-Rain.json").unwrap(),
+            _ => panic!("Lol aint no way"),
+        },
+        6 => File::open("scenes/June-Clear.json").unwrap(),
+        7 => match rand % 2 {
+            0 => File::open("scenes/July-Clear.json").unwrap(),
+            1 => File::open("scenes/July-Cloudy.json").unwrap(),
+            _ => panic!("Lol aint no way"),
+        },
+        8 => File::open("scenes/August-Clear.json").unwrap(),
+        9 => match rand % 2 {
+            0 => File::open("scenes/September-Clear.json").unwrap(),
+            1 => File::open("scenes/September-Cloudy.json").unwrap(),
+            _ => panic!("Lol aint no way"),
+        },
+        10 => match rand % 3 {
+            0 => File::open("scenes/EarlyOctober-Clear.json").unwrap(),
+            1 => File::open("scenes/LateOctober-Clear.json").unwrap(),
+            2 => File::open("scenes/LateOctober-Rain.json").unwrap(),
+            _ => panic!("Lol aint no way"),
+        },
+        11 => match rand % 2 {
+            0 => File::open("scenes/November-Clear.json").unwrap(),
+            1 => File::open("scenes/November-Rain.json").unwrap(),
+            _ => panic!("Lol aint no way"),
+        },
+        12 => File::open("scenes/December-Clear.json").unwrap(),
+        _ => panic!("Lol aint no way"),
+    };
     let mut contents: String = String::new();
     _ = file.read_to_string(&mut contents);
 
@@ -61,23 +88,60 @@ fn main() -> std::io::Result<()> {
         .members()
         .map(|x: &JsonValue| x.as_u8().unwrap())
         .collect();
-    println!("{} x {}", width, height);
 
-    let palettes: JsonValue = parsed["palettes"].clone();
-
-    let timeline: BTreeMap<u32, &str> = parsed["timeline"]
+    let timeline: BTreeMap<u32, String> = parsed["timeline"]
         .entries()
-        .map(|(time, palette)| (time.parse::<u32>().unwrap(), palette.as_str().unwrap()))
+        .map(|(time, palette)| {
+            (
+                time.parse::<u32>().unwrap(),
+                palette.as_str().unwrap().to_string(),
+            )
+        })
         .collect();
 
-    let now = Local::now();
-    let seconds = now.num_seconds_from_midnight();
-
-    let palette = get_palette(&palettes, &timeline, seconds);
+    let palettes_raw: JsonValue = parsed["palettes"].clone();
+    let mut palettes: HashMap<String, [[u8; 3]; 256]> = HashMap::new();
+    let mut cycles: HashMap<String, Vec<Cycle>> = HashMap::new();
+    let cycle_speed = 280;
+    for (name, value) in palettes_raw.entries() {
+        let cycles_vec: Vec<Cycle> = value["cycles"]
+            .members()
+            .map(|value: &JsonValue| {
+                let reverse: u32 = value["reverse"].as_u32().unwrap();
+                let rate: u32 = value["rate"].as_u32().unwrap();
+                let low: u32 = value["low"].as_u32().unwrap();
+                let high: u32 = value["high"].as_u32().unwrap();
+                Cycle {
+                    reverse,
+                    rate,
+                    low,
+                    high,
+                }
+            })
+            .collect();
+        cycles.insert(name.to_string(), cycles_vec);
+        let colors = value["colors"]
+            .members()
+            .map(|x| {
+                let r = x[0].as_u8().unwrap();
+                let g = x[1].as_u8().unwrap();
+                let b = x[2].as_u8().unwrap();
+                [r, g, b]
+            })
+            .collect::<Vec<[u8; 3]>>();
+        let mut palette = [[0; 3]; 256];
+        for (i, color) in colors.iter().enumerate() {
+            palette[i] = *color;
+        }
+        palettes.insert(name.to_string(), palette);
+    }
 
     // GL
     let event_loop = EventLoop::new();
-    let window_builder = WindowBuilder::new().with_inner_size(PhysicalSize::new(800, 600));
+    let window_builder = WindowBuilder::new()
+        .with_inner_size(PhysicalSize::new(width, height))
+        .with_title("living-worlds")
+        .with_decorations(false);
     let context_builder = ContextBuilder::new().with_vsync(true);
     let gl_window = context_builder
         .build_windowed(window_builder, &event_loop)
@@ -111,14 +175,10 @@ fn main() -> std::io::Result<()> {
             out vec4 fragColor;
     
             void main() {    
-                // Flip the y coordinate
                 vec2 flippedUV = vec2(uv.x, 1.0 - uv.y);
-                // Get the color index of the current fragment
                 float index = texture(color_indices, flippedUV).r;
-                // Get the color from the palette using the index
                 vec3 color = texture(palette, index).rgb;
     
-                // Set the final fragment color
                 fragColor = vec4(color, 1.0);
             }
         "#;
@@ -172,7 +232,7 @@ fn main() -> std::io::Result<()> {
         gl::TexImage2D(
             gl::TEXTURE_2D,
             0,
-            gl::R8 as i32, // Assuming color index data is single-channel
+            gl::R8 as i32,
             width as i32,
             height as i32,
             0,
@@ -190,16 +250,6 @@ fn main() -> std::io::Result<()> {
         // Create the palette texture
         gl::GenTextures(1, &mut palette_texture);
         gl::BindTexture(gl::TEXTURE_1D, palette_texture);
-        gl::TexImage1D(
-            gl::TEXTURE_1D,
-            0,
-            gl::RGB8 as i32,
-            256,
-            0,
-            gl::RGB,
-            gl::UNSIGNED_BYTE,
-            palette.as_ptr() as *const _,
-        );
 
         // Set texture parameters
         gl::TexParameteri(gl::TEXTURE_1D, gl::TEXTURE_WRAP_S, gl::CLAMP_TO_EDGE as i32);
@@ -272,6 +322,67 @@ fn main() -> std::io::Result<()> {
             _ => (),
         }
 
+        let now = Local::now();
+        let seconds = now.num_seconds_from_midnight();
+        let milliseconds = std::u32::MAX - 100 * now.timestamp_millis() as u32;
+        let time = get_palette_time(&timeline, seconds);
+        let palette_id = &timeline[&time];
+        let mut palette = palettes[palette_id].clone();
+        let cycles = &cycles[palette_id];
+
+        for cycle in cycles {
+            if cycle.rate != 0 {
+                let cycle_size = (cycle.high - cycle.low) + 1;
+                let cycle_rate = cycle.rate / cycle_speed;
+                let mut cycle_amount = 0;
+
+                if cycle.reverse < 3 {
+                    cycle_amount = (milliseconds / 1000 / cycle_rate) % cycle_size
+                } else if cycle.reverse == 3 {
+                    cycle_amount = (milliseconds / 1000 / cycle_rate) % (cycle_size * 2);
+                    if cycle_amount >= cycle_size {
+                        cycle_amount = cycle_size * 2 - cycle_amount;
+                    }
+                } else if cycle.reverse < 6 {
+                    cycle_amount = (milliseconds / 1000 / cycle_rate) % cycle_size;
+                    cycle_amount = (((cycle_amount as f32 * std::f32::consts::PI * 2.0)
+                        / cycle_size as f32)
+                        .sin()
+                        + 1.0) as u32;
+                    if cycle.reverse == 4 {
+                        cycle_amount *= cycle_size / 4;
+                    } else if cycle.reverse == 5 {
+                        cycle_amount *= cycle_size / 2;
+                    }
+                }
+
+                if cycle.reverse == 2 {
+                    for i in 0..cycle_amount {
+                        let temp = palette[cycle.low as usize + i as usize].clone();
+                        palette[cycle.low as usize + i as usize] =
+                            palette[cycle.high as usize - i as usize].clone();
+                        palette[cycle.high as usize - i as usize] = temp;
+                    }
+                }
+                for _ in 0..cycle_amount {
+                    let temp = palette[cycle.low as usize].clone();
+                    for j in cycle.low..=cycle.high - 1 {
+                        let j = j as usize;
+                        palette[j] = palette[j + 1].clone();
+                    }
+                    palette[cycle.high as usize] = temp;
+                }
+                if cycle.reverse == 2 {
+                    for i in 0..cycle_amount {
+                        let temp = palette[cycle.low as usize + i as usize].clone();
+                        palette[cycle.low as usize + i as usize] =
+                            palette[cycle.high as usize - i as usize].clone();
+                        palette[cycle.high as usize - i as usize] = temp;
+                    }
+                }
+            }
+        }
+
         unsafe {
             // Activate the shader program
             gl::UseProgram(shader_program);
@@ -288,6 +399,17 @@ fn main() -> std::io::Result<()> {
             // Bind textures
             gl::ActiveTexture(gl::TEXTURE0);
             gl::BindTexture(gl::TEXTURE_1D, palette_texture);
+            gl::TexImage1D(
+                gl::TEXTURE_1D,
+                0,
+                gl::RGB8 as i32,
+                256,
+                0,
+                gl::RGB,
+                gl::UNSIGNED_BYTE,
+                palette.as_ptr() as *const _,
+            );
+
             gl::ActiveTexture(gl::TEXTURE1);
             gl::BindTexture(gl::TEXTURE_2D, color_indices_texture);
 
@@ -336,4 +458,15 @@ fn compile_shader(source: &str, shader_type: gl::types::GLenum) -> gl::types::GL
     }
 
     shader
+}
+
+fn get_palette_time(timeline: &BTreeMap<u32, String>, seconds: u32) -> u32 {
+    let mut last_time: &u32 = timeline.keys().last().unwrap();
+    for (time, _) in timeline.iter() {
+        if time > &seconds {
+            return *last_time;
+        }
+        last_time = time;
+    }
+    *timeline.keys().last().unwrap()
 }
